34aca9c47af7b8ae29eab071a0e3943b0689ac49
NafiAsib <nafi.asib@gmail.com>
2021-07-19 11:10:39 +0600

    added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+fish_vi_key_bindings
+
+abbr desktop cd /mnt/c/Users/nafi/Desktop
+abbr ls ls -la
+abbr dev cd ~/dev
+abbr solve cd ~/problem-solving
+export PATH="/home/nafi/.local/bin:$PATH"
---
+++ .config/fish/functions/fish_greeting.fish
@@ -0,0 +1,3 @@
+function fish_greeting
+	printf (set_color F90)"Let's Hack Asib!\n"
+end
